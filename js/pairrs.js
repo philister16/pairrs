b6f092313e6f34cb414b73c4da0fe5a4351d4f2d
Philipp Nueesch <phil@rhinerock.com>
2014-10-14 08:55:16 +0800

    Built out shuffleCards method randomizing cards with Fisher-Yates Updated html string to include images wihtin <div class='card-down'> Updated distributeCards for loop to loop through image stack Loop now assigns an id equivalent to its index in imageStack array to its card <div> and also assigns a name attribute equivalent to its id property in the collection to the same card <div> Introduced a new game property on the main pairrs app to hold the logic for the gameplay Implemented game.state to keep the states of the game
    playStroke method to handle the actual player action flipping cards and searching for pairs. Includes a switch to determine if player flips first or second card in attempt to find pair.

--- js/pairrs.js
+++ js/pairrs.js
@@ -15,59 +15,80 @@
  */
 var pairrs = {
   
-/**
- * Holds the content aka cards that are loaded to play with
- * @type {obj}
- */
+  /**
+   * Holds the content aka cards that are loaded to play with
+   * @type {obj}
+   */
   content : {},
   
-/**
- * Loads the desired set of cards into the content property and return the content property
- * @param {obj} collection object as parsed from JSON
- * @return {obj} the content property populated with desired collection
- */
+  /**
+   * Loads the desired set of cards into the content property and return the content property
+   * @param {obj} collection object as parsed from JSON
+   * @return {obj} the content property populated with desired collection
+   */
   load : function(collection) {
     return this.content = collection;
   },
 
-/**
- * Empties the content property and returns the property as an empty object
- * @return {obj} empty content object
- */
+  /**
+   * Empties the content property and returns the property as an empty object
+   * @return {obj} empty content object
+   */
   unload : function() {
     return this.content = {};
   },
   
-/**
- * Loads all images from content object twice into an array, randomly shuffles this array and returns it
- * @param {arr} content.images array 
- * @return {arr} randomized array containing 30 objects
- */ 
+  /**
+   * Loads all images from content object twice into an array, randomly shuffles this array and returns it
+   * @param {arr} content.images array 
+   * @return {arr} randomized array containing 30 objects
+   */ 
   shuffleCards : function(content) {
     var imageStack = [];
     for(var i = 0; i < content.length; i++) {
       imageStack.push(content[i]);
       imageStack.push(content[i]); // in the content each image occurs once, but we need it twice to create pairs
     }
+    
+    imageStack = shuffle(imageStack);
     return imageStack;
     
-    // todo shuffle / randomize imageStack before return
+    /**
+     * Randomly shuffles the array based on the Fisher-Yates Shuffle algorithm
+     * @param {arr} unshuffled array
+     * @return {arr} randomly shuffled array
+     */
+    function shuffle(array) {
+      var counter = array.length, temp, index;
+      // While there are elements in the array
+      while (counter > 0) {
+          // Pick a random index
+          index = Math.floor(Math.random() * counter);
+          // Decrease counter by 1
+          counter--;
+          // And swap the last element with it
+          temp = array[counter];
+          array[counter] = array[index];
+          array[index] = temp;
+      }
+      return array;
+    }
     
   },
   
-/**
- * Builds up html string, loops through imageStack array and finally appends html to the main content
-    area 30x for each card/image in the imageStack array
- * @param {arr} imageStack array which is a result of {func} shuffleCards 
- * @return {bool} true
- */   
-  distributeCards : function() {
+  /**
+   * Builds up html string, loops through imageStack array and finally appends html to the main content
+      area 30x for each card/image in the imageStack array
+   * @param {arr} imageStack array which is a result of {func} shuffleCards 
+   * @return {bool} true
+   */   
+  distributeCards : function(images) {
     
     var cards = [];
     
     // fill 30 cards into array
-    for(var i = 0; i < 30; i++) {
-      cards.push("<div class='col-xs-2'><section class='cardholder'><div class='card' onclick='pairrs.flipCard(this)'><div class='card-down'>Front</div><div class='card-up'><img src='./collections/"+this.content.name+"/0_backside.jpg' class='img-responsive'></div></div></section></div>");
+    for(var i = 0; i < images.length; i++) {
+      cards.push("<div class='col-xs-2'><section class='cardholder'><div name='"+images[i].id+"' id='"+i+"' class='card' onclick='pairrs.game.playStroke(this)'><div class='card-down'><img src='./collections/"+this.content.name+"/"+images[i].file+"' class='img-responsive'></div><div class='card-up'><img src='./collections/"+this.content.name+"/0_backside.jpg' class='img-responsive'></div></div></section></div>");
     }
     
     // create 5 row div and insert 6 cards each
@@ -81,21 +102,104 @@ var pairrs = {
     return true;
   },
   
-/**
- * Triggers CSS transition to 3D flip around individual card
- * @param {elem} clicked html element (div.card) 
- * @return {bool} true
- */    
+  /**
+   * Single game play
+   * @type {obj}
+   */
+  game : {
+    
+    /**
+     * Current status of the game
+     * @type {obj}
+     */
+    state : {
+      ongoing : true, // as long as not all pairs are found game is ongoing
+      secondCard : false, // either we flip the 1st card or the 2nd card
+      currentCards : [{ name : "", id : "" },{ name : "", id : "" }],
+      wonPairs : 0
+    },
+    
+    /**
+     * Executes a stroke in the game
+     * @param {elem} html element clicked (div.card)
+     * @return {arr} array with status values to update game.status obj
+     */
+    playStroke : function(elem) {
+      switch(this.state.secondCard) {
+          
+        case false :
+          
+          pairrs.flipCard(elem); // flip the card clicked
+          this.state.secondCard = true; // next time we flip the 2nd card
+          
+          // load the card's id and name into currentCards array
+          this.state.currentCards[0].name = $(elem).attr("name");
+          this.state.currentCards[0].id = $(elem).attr("id");
+          break;
+          
+        case true :
+          
+          var clickedCard = $(elem).attr("id");
+          
+          // if the 2nd clicked card is not the same as the one in the first stroke flip it
+          if(clickedCard !== this.state.currentCards[0].id) {
+            pairrs.flipCard(elem);
+            
+            // load the card's id and name into currentCards array
+            this.state.currentCards[1].name = $(elem).attr("name");
+            this.state.currentCards[1].id = $(elem).attr("id");
+            
+            // if the names of the two flipped cards match player wins
+            if(this.state.currentCards[0].name === this.state.currentCards[1].name) {
+              this.state.wonPairs += 1;
+              $(this.state.currentCards[0].id).removeAttr("onclick"); // remove the click event as pair is won
+              $(this.state.currentCards[1].id).removeAttr("onclick"); // remove the click event as pair is won
+            } else {
+            // flip back the 2 non-matching cards after 3 seconds
+              var elem1 = document.getElementById(this.state.currentCards[0].id);
+              var elem2 = document.getElementById(this.state.currentCards[1].id);
+              setTimeout(function(elem1, elem2) {
+                pairrs.flipCard(elem1);
+                pairrs.flipCard(elem2);
+              }, 3000);
+            }
+            this.state.currentCards = [{ name : "", id : "" },{ name : "", id : "" }]; // init the array again
+            this.state.secondCard = false; // next time we flip will be the 1st card again
+            
+            // if we won all 15 pairs the game is over
+            if(this.state.wonPairs === 15) {
+              this.state.ongoing = false;
+            }
+          }
+          break;
+      }
+    }
+    
+  },
+  
+  /**
+   * Triggers CSS transition to 3D flip around individual card
+   * @param {elem} clicked html element (div.card) 
+   * @return {bool} true
+   */
   flipCard : function(element) {
     $(element).toggleClass("flipped");
     return true;
   },
-  
+
+/**
+ * Triggers CSS transition to 3D flip (= add class .flipped) all cards 
+ * @return {bool} true
+ */
   flipAll : function() {
     $(".card").addClass("flipped");
     return true;
   },
   
+/**
+ * Triggers CSS transition to 3D unflip (= remove class .flipped) all cards 
+ * @return {bool} true
+ */
   unflipAll : function() {
     $(".card").removeClass("flipped");
     return true;
