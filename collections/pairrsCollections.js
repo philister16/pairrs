de2b5e8364fae0c449555a29d9b0c57cf00b7b12
Philipp Nueesch <phil@rhinerock.com>
2014-11-17 21:30:34 +0800

    Cleaned up collections

--- collections/pairrsCollections.js
+++ collections/pairrsCollections.js
@@ -5,30 +5,6 @@ var pairrsCollections = {
   cardDecks : [
     {
       "id" : 0, // will be updated dynamically on load through function pairrsCollection_populateId
-      "name" : "All My Friends",
-      "folder" : "pairrsColl_AllMyFriends",
-      "cover" : "0_cover.png",
-      "backside" : "0_backside.png",
-      "images" : [
-        { "id" : 1, "file" : "1_ancient.jpg" },
-        { "id" : 2, "file" : "2_beach.jpg" },
-        { "id" : 3, "file" : "3_bike.jpg" },
-        { "id" : 4, "file" : "4_building.jpg" },
-        { "id" : 5, "file" : "5_cars.jpg" },
-        { "id" : 6, "file" : "6_colmar.jpg" },
-        { "id" : 7, "file" : "7_dawn.jpg" },
-        { "id" : 8, "file" : "8_east.jpg" },
-        { "id" : 9, "file" : "9_gracht.jpg" },
-        { "id" : 10, "file" : "10_harbor.jpg" },
-        { "id" : 11, "file" : "11_milano.jpg" },
-        { "id" : 12, "file" : "12_riga.jpg" },
-        { "id" : 13, "file" : "13_square.jpg" },
-        { "id" : 14, "file" : "14_station.jpg" },
-        { "id" : 15, "file" : "15_steine.jpg" }
-      ]
-    },
-    {
-      "id" : 0, // will be updated dynamically on load through function pairrsCollection_populateId
       "name" : "Ancient Cars",
       "folder" : "pairrsColl_Ancars",
       "cover" : "0_cover.png",
